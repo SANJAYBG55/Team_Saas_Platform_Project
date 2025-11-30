26b029d150b828d44164e8a040f44324671d8ca2
SANJAY B G <sanjaysanju143216@gmail.com>
2025-11-30 09:59:35 +0530

    12th commit

--- config/settings.py
+++ config/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'static']
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'  # Whitenoise not installed
 
 # Media files
 MEDIA_URL = '/media/'
